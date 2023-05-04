185005d73c273cf352062905e3f22329b65c1dba
Kayrat Ayazov <ayazov.k@yandex.ru>
2023-05-04 21:01:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.function.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);
+        int c = calc.devide.apply(a, b);
+
+        calc.println.accept(c);
+    }
+}
+//ошибка, т.к. функция devide принимает в качестве делителя результат функции minus равный нулю.
+//
+
+class Calculator {
+    static Supplier<Calculator> instance = Calculator::new;
+    BinaryOperator<Integer> plus = Integer::sum;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> devide = (x, y) -> {
+        if (y != 0) {
+            return x / y;
+        } else {
+            System.out.println("Деление на ноль!!!");
+
+            return null;
+        }
+    };
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+
+    Predicate<Integer> isPositive = x -> x > 0;
+
+    Consumer<Integer> println = System.out::println;
+}
